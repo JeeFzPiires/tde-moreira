aed5726fd0b9f6775078585a820cfa5c8b1582cb
JeeFzPiires <jeferson.piires@gmail.com>
2022-09-11 19:05:35 -0300

    Atualizando

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,19 +14,25 @@ export default function Home() {
             <div className="px-4 py-6 sm:px-0">
               <h1 className='text-white text-xl mb-2'>O que é uma Gestão Estratégica de TI?</h1>
               <div className="border-2 border-[#28edf5] border-solid bg-slate-300 shadow-md shadow-black rounded-lg h-96 sm:h-min">
-                <h4 className='mx-2 mt-2 text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo dolorem, provident nam rerum dolores atque. Fuga dolore ex fugiat unde! Sapiente nostrum quam vero placeat necessitatibus ab! Corporis, odit.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perspiciatis deserunt expedita deleniti iure iste consequuntur accusamus dicta natus nostrum, explicabo corporis fugit commodi quo reiciendis delectus incidunt! Labore, minima!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt similique doloremque maiores officia alias, voluptates unde, modi soluta quod, quibusdam neque! Nihil, doloribus! Nemo sequi quidem cumque eaque accusantium?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quis rerum nihil et ut animi in, eaque perferendis quidem ratione praesentium cumque molestias voluptatibus ipsam error. Minima optio harum vitae?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptas tenetur explicabo, quaerat nisi earum, quas accusantium ullam at sapiente dolorum iusto placeat aspernatur enim quasi quos, ipsa alias dolorem!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum, placeat maiores doloribus aut tempora delectus. Harum, reiciendis dignissimos? Possimus, adipisci soluta reiciendis nobis consectetur quos assumenda dicta aut autem.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde error distinctio officiis est nulla sed eius? Sequi voluptatem optio facere quasi commodi. Odio dolor aliquam reprehenderit neque voluptas optio repellendus.
-                </h4>
+                <p className='mx-2 mt-2 text-black indent-8 md:indent-4 sm:indent-4 leading-relaxed whitespace-pre-line break-words mb-4'>
+                A gestão estratégica é um conjunto de práticas aplicadas na administração de uma empresa, departamento e até mesmo de projetos, 
+                em que os processos são estruturados de forma eficaz para atingir os objetivos traçados pela organização. Por meio dela, 
+                todas as tarefas são executadas em cima de direcionamentos previamente estabelecidos, ajudando a impulsionar a competitividade e o crescimento do negócio.  
+                <br/><p>A gestão de TI é definida por todas as práticas voltadas para a administração dos recursos e soluções de tecnologia de uma empresa.
+                Quando o gerenciamento é pensado e feito de forma estratégica ele assume condutas de governança sobre as ferramentas da área com o objetivo de melhorar os resultados da companhia.  
+                </p>
+                <p>O TI estratégico consegue direcionar as ações para maior economia de recursos e eficiência integral das equipes, 
+                podendo promover a geração de incremento de vendas, melhoria de serviços, produtos, atendimento e diversos outros benefícios. 
+                Com profissionais engajados e capacitados, sobrepujando práticas de apenas fornecer suporte, recursos ou ferramentas, 
+                é possível que o setor de TI se torne um ativo importante para os projetos da companhia. 
+                Para isso, é necessário que os objetivos da área estejam em consonância com os da instituição.</p>
+                <br/>
+                Fonte: <a class="text-sky-400 hover:after:content-['_↗'] ..." href="https://www.findup.com.br/gestao-de-ti-estrategica/" target="_blank">https://www.findup.com.br/gestao-de-ti-estrategica/</a>
+                </p>
               </div>
               <h1 className='text-white text-xl mt-8 mb-2'>Resenha sobre Gestão Estratégica de TI</h1>
               <div className="border-2 border-[#28edf5] border-solid bg-slate-300 shadow-md shadow-black rounded-lg h-96 sm:h-min">
-                <h4 className='mx-2 mt-2 text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
+                <p className='mx-2 mt-2 mb-4 text-black indent-8 md:indent-4 sm:indent-4 leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo dolorem, provident nam rerum dolores atque. Fuga dolore ex fugiat unde! Sapiente nostrum quam vero placeat necessitatibus ab! Corporis, odit.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perspiciatis deserunt expedita deleniti iure iste consequuntur accusamus dicta natus nostrum, explicabo corporis fugit commodi quo reiciendis delectus incidunt! Labore, minima!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt similique doloremque maiores officia alias, voluptates unde, modi soluta quod, quibusdam neque! Nihil, doloribus! Nemo sequi quidem cumque eaque accusantium?
@@ -35,11 +41,11 @@ export default function Home() {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum, placeat maiores doloribus aut tempora delectus. Harum, reiciendis dignissimos? Possimus, adipisci soluta reiciendis nobis consectetur quos assumenda dicta aut autem.
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde error distinctio officiis est nulla sed eius? Sequi voluptatem optio facere quasi commodi. Odio dolor aliquam reprehenderit neque voluptas optio repellendus.
 
-                </h4>
+                </p>
               </div>
               <h1 className='text-white text-xl mt-8 mb-2'>Quais benefícios da Gestão Estratégica de TI?</h1>
               <div className="border-2 border-[#28edf5]  border-solid bg-slate-300 shadow-md shadow-black rounded-lg h-96 sm:h-min">
-                <h4 className='mx-2 mt-2 text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
+                <p className='mx-2 mt-2 mb-4 text-black indent-8 md:indent-4 sm:indent-4 leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo dolorem, provident nam rerum dolores atque. Fuga dolore ex fugiat unde! Sapiente nostrum quam vero placeat necessitatibus ab! Corporis, odit.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perspiciatis deserunt expedita deleniti iure iste consequuntur accusamus dicta natus nostrum, explicabo corporis fugit commodi quo reiciendis delectus incidunt! Labore, minima!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt similique doloremque maiores officia alias, voluptates unde, modi soluta quod, quibusdam neque! Nihil, doloribus! Nemo sequi quidem cumque eaque accusantium?
@@ -47,11 +53,11 @@ export default function Home() {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptas tenetur explicabo, quaerat nisi earum, quas accusantium ullam at sapiente dolorum iusto placeat aspernatur enim quasi quos, ipsa alias dolorem!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum, placeat maiores doloribus aut tempora delectus. Harum, reiciendis dignissimos? Possimus, adipisci soluta reiciendis nobis consectetur quos assumenda dicta aut autem.
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde error distinctio officiis est nulla sed eius? Sequi voluptatem optio facere quasi commodi. Odio dolor aliquam reprehenderit neque voluptas optio repellendus.
-                </h4>
+                </p>
               </div>
-              <h1 className='text-white text-xl mt-8 mb-2'>Entrevista com Geraldo Dutra - Gestor de TI</h1>
+              {/* <h1 className='text-white text-xl mt-8 mb-2'>Entrevista com Geraldo Dutra - Gestor de TI</h1>
               <div className="border-2 border-[#28edf5]  border-solid bg-slate-300 shadow-md shadow-black rounded-lg h-96 sm:h-min">
-                <h4 className='mx-2 mt-2 text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
+                <p className='mx-2 mt-2 text-black leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo dolorem, provident nam rerum dolores atque. Fuga dolore ex fugiat unde! Sapiente nostrum quam vero placeat necessitatibus ab! Corporis, odit.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perspiciatis deserunt expedita deleniti iure iste consequuntur accusamus dicta natus nostrum, explicabo corporis fugit commodi quo reiciendis delectus incidunt! Labore, minima!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt similique doloremque maiores officia alias, voluptates unde, modi soluta quod, quibusdam neque! Nihil, doloribus! Nemo sequi quidem cumque eaque accusantium?
@@ -59,8 +65,8 @@ export default function Home() {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptas tenetur explicabo, quaerat nisi earum, quas accusantium ullam at sapiente dolorum iusto placeat aspernatur enim quasi quos, ipsa alias dolorem!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum, placeat maiores doloribus aut tempora delectus. Harum, reiciendis dignissimos? Possimus, adipisci soluta reiciendis nobis consectetur quos assumenda dicta aut autem.
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde error distinctio officiis est nulla sed eius? Sequi voluptatem optio facere quasi commodi. Odio dolor aliquam reprehenderit neque voluptas optio repellendus.
-                </h4>
-              </div>
+                </p>
+              </div> */}
             </div>
           </div>
         </main>
