e7f6457b6df359f8e65d24d4af86fd5af7e0a3cf
JeeFzPiires <jeferson.piires@gmail.com>
2022-09-12 18:48:17 -0300

    Resenha

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -32,27 +32,76 @@ export default function Home() {
               </div>
               <h1 className='text-white text-xl mt-8 mb-2'>Resenha sobre Gestão Estratégica de TI</h1>
               <div className="border-2 border-[#28edf5] border-solid bg-slate-300 shadow-md shadow-black rounded-lg h-96 sm:h-min">
-                <p className='mx-2 mt-2 mb-4 text-black indent-8 md:indent-4 sm:indent-4 leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo dolorem, provident nam rerum dolores atque. Fuga dolore ex fugiat unde! Sapiente nostrum quam vero placeat necessitatibus ab! Corporis, odit.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perspiciatis deserunt expedita deleniti iure iste consequuntur accusamus dicta natus nostrum, explicabo corporis fugit commodi quo reiciendis delectus incidunt! Labore, minima!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt similique doloremque maiores officia alias, voluptates unde, modi soluta quod, quibusdam neque! Nihil, doloribus! Nemo sequi quidem cumque eaque accusantium?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quis rerum nihil et ut animi in, eaque perferendis quidem ratione praesentium cumque molestias voluptatibus ipsam error. Minima optio harum vitae?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptas tenetur explicabo, quaerat nisi earum, quas accusantium ullam at sapiente dolorum iusto placeat aspernatur enim quasi quos, ipsa alias dolorem!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum, placeat maiores doloribus aut tempora delectus. Harum, reiciendis dignissimos? Possimus, adipisci soluta reiciendis nobis consectetur quos assumenda dicta aut autem.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde error distinctio officiis est nulla sed eius? Sequi voluptatem optio facere quasi commodi. Odio dolor aliquam reprehenderit neque voluptas optio repellendus.
+                <p className='mx-2 mt-2 mb-4 text-black indent-8 md:indent-4 sm:indent-4 leading-relaxed'>
+                  Gestão estratégica é um assunto muito importante no mercado atual, o mesmo aborda a importância da gestão do conhecimento utilizando ferramentas que a TI pode oferecer. 
+                  O mundo está passando por uma evolução da tecnologia muito rápido, em uma grande escala, e para o crescimento das empresas hoje em dia tornou-se muito importante,
+                  juntamente com a gestão pode-se obter um lucro muito interessante aos empreendedores.
 
                 </p>
               </div>
               <h1 className='text-white text-xl mt-8 mb-2'>Quais benefícios da Gestão Estratégica de TI?</h1>
               <div className="border-2 border-[#28edf5]  border-solid bg-slate-300 shadow-md shadow-black rounded-lg h-96 sm:h-min">
-                <p className='mx-2 mt-2 mb-4 text-black indent-8 md:indent-4 sm:indent-4 leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, exercitationem ea. Adipisci ad, a voluptas corrupti deserunt fugiat odit minus enim quae ipsam magni tenetur similique, distinctio quas dolorum saepe.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo dolorem, provident nam rerum dolores atque. Fuga dolore ex fugiat unde! Sapiente nostrum quam vero placeat necessitatibus ab! Corporis, odit.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis perspiciatis deserunt expedita deleniti iure iste consequuntur accusamus dicta natus nostrum, explicabo corporis fugit commodi quo reiciendis delectus incidunt! Labore, minima!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt similique doloremque maiores officia alias, voluptates unde, modi soluta quod, quibusdam neque! Nihil, doloribus! Nemo sequi quidem cumque eaque accusantium?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quis rerum nihil et ut animi in, eaque perferendis quidem ratione praesentium cumque molestias voluptatibus ipsam error. Minima optio harum vitae?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptas tenetur explicabo, quaerat nisi earum, quas accusantium ullam at sapiente dolorum iusto placeat aspernatur enim quasi quos, ipsa alias dolorem!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum, placeat maiores doloribus aut tempora delectus. Harum, reiciendis dignissimos? Possimus, adipisci soluta reiciendis nobis consectetur quos assumenda dicta aut autem.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde error distinctio officiis est nulla sed eius? Sequi voluptatem optio facere quasi commodi. Odio dolor aliquam reprehenderit neque voluptas optio repellendus.
+                <p className='mx-2 mt-2 mb-4 text-black indent-8 md:indent-4 sm:indent-4 leading-relaxed'>
+                  <ul>
+                    <li className='font-bold text-lg'>Redução de custos</li>
+                    <p>
+                    Com o setor de TI estratégico atuando, é possível reduzir custos de várias formas. 
+                    Uma delas é ao realizar investimentos em novos equipamentos. 
+                    Em vez de comprar um padrão para todos, a área primeiro analisa a usabilidade de cada colaborador, 
+                    define o perfil de hardware necessário e, apenas depois disso, os equipamentos são comprados. 
+                    Desse modo, as necessidades de uso e performance são garantidas, mas com uma necessidade de investimento consideravelmente menor.
+                    </p><br/>
+                    <li className='font-bold text-lg'>Acompanhamento de inovação do mercado</li>
+                    <p>
+                    Com o departamento de TI tendo maior liberdade e tomando parte das decisões mais importantes da empresa, 
+                    a área terá obrigação de estar inteirada com as inovações do mercado. 
+                    Esse conhecimento será importante para que o departamento consiga oferecer um terreno sólido para que os outros setores possam aumentar a taxa de sucesso de suas decisões.
+                    Com profissionais alinhados às tendências e às novas tecnologias, a TI Estratégica poderá apontar propostas e caminhos para a empresa, como:
+                    <ul className='list-inside list-disc'>
+                      <li>quais produtos abandonar</li>
+                      <li>áreas que devem receber mais investimento para inovação</li>
+                      <li>como e onde se comunicar com o público</li>
+                    </ul>
+                    </p><br/>
+                    <li className='font-bold text-lg'>Garantia de um ambiente seguro</li>
+                    <p>
+                    Depois dos colaboradores, a informação é um dos fatores mais preciosos para qualquer organização. 
+                    É por esse motivo que a segurança é uma das principais responsabilidades da TI estratégia. 
+                    Eles são responsáveis por homologar os softwares utilizados, gerir as permissões de acesso e treinar as equipes.
+                    </p><p>
+                    Com essas e outras ações, eles garantem a consolidação de um ambiente seguro para a troca de informações e 
+                    atuam de forma preventiva para que a empresa não tenha as suas operações interrompidas ou seus dados roubados.
+                    </p><br/>
+                    <li className='font-bold text-lg'>Melhor monitoramento das equipes</li>
+                    <p>
+                    Como dito por William Edwards Deming: “Não há sucesso no que não se gerencia”, 
+                    e um dos principais fatores para o gerenciamento eficaz é o monitoramento. É para suprir essa necessidade que uma TI Estratégica pode ser um grande diferencial.
+                    </p><p>
+                    Só o departamento de tecnologia é capaz de criar e implementar ferramentas que melhorem e facilitem o monitoramento das equipes de forma ampla e concreta. 
+                    Além disso, tendo a sua capacidade produtiva aumentada, o setor de TI poderá oferecer dados mais coesos das equipes.
+                    </p><br/>
+                    <li className='font-bold text-lg'>Geração de Sinergia entre as áreas</li>
+                    <p>
+                    Como a TI estratégica está diretamente ligada ao planejamento corporativo, todas as áreas poderão usufruir dos seus benefícios. 
+                    Além disso, o cruzamento das informações pode gerar insights relevantes, aumentar a colaboração entre as áreas e, por meio da sinergia, alavancar os resultados.
+                    </p><br/>
+                    <li className='font-bold text-lg'>Desenvolvimento de uma cultura de inovação</li>
+                    <p>
+                    A empresa que aplica a TI estratégica incute nos seus colaboradores uma cultura de inovação e melhoria contínua, 
+                    além de mostrar que a sua atividade agrega valor para toda a sociedade. Esse fato é muito relevante, 
+                    pois colaboradores com propósito produzem mais, são muito comprometidos e permanecem mais tempo na empresa.
+                    </p><p>
+                    E os benefícios não ficam apenas dentro da empresa. Outra vantagem da cultura da inovação é ganhar uma legião de fãs 
+                    que aguardam ansiosamente pelos lançamentos de novos produtos e serviços, que geralmente são vendidos por preços mais elevados, 
+                    pois o cliente considera o valor agregado muito maior que o preço.
+                    </p><p>
+                    A TI estratégica é essencial para as organizações que desejam alavancar os seus resultados. 
+                    Por isso, deve estar diretamente ligada à estratégia de negócios e interagir com todas as áreas da empresa. 
+                    Se bem implantada, poderá melhorar os processos, reduzir custos, otimizar a comunicação, melhorar o nível de serviço, 
+                    atrair novos cliente e aumentar a rentabilidade da empresa.
+                    </p><br/>
+                    Fonte: <a class="text-sky-400 hover:after:content-['_↗'] ..." href="https://blog.centralserver.com.br/o-que-e-ti-estrategica-e-quais-suas-vantagens-para-sua-empresa/" target="_blank">https://blog.centralserver.com.br/o-que-e-ti-estrategica-e-quais-suas-vantagens-para-sua-empresa/</a>
+                  </ul>
                 </p>
               </div>
               {/* <h1 className='text-white text-xl mt-8 mb-2'>Entrevista com Geraldo Dutra - Gestor de TI</h1>
